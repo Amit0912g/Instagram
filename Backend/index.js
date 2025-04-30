3a1432d4b3b6866a2402cfcb8a2fbba9823b3fc0
Amit Gupta <amit.g09122000@gmail.com>
2025-04-30 18:37:00 +0530

    cors

--- Backend/index.js
+++ Backend/index.js
@@ -19,10 +19,12 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 const corsOptions={
     origin: [
-        "http://localhost:5173",
-        "https://instagram-liart-zeta.vercel.app"
+        "https://instagram-liart-zeta.vercel.app",
+        "http://localhost:5173"
       ],
-    credentials:true,
+      credentials: true,         
+  methods: ["GET", "POST", "PUT", "DELETE"], 
+  allowedHeaders: ["Content-Type", "Authorization"],
 }
 app.use(cors(corsOptions))
 
