262a7bb8018de24f876398b2171c1d3b5d5266eb
Amit Gupta <amit.g09122000@gmail.com>
2025-04-30 18:02:47 +0530

    cors handling

--- Backend/index.js
+++ Backend/index.js
@@ -18,7 +18,10 @@ app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 const corsOptions={
-    origin:"http://localhost:5173",
+    origin: [
+        "http://localhost:5173",
+        "https://instagram-liart-zeta.vercel.app"
+      ],
     credentials:true,
 }
 app.use(cors(corsOptions))
--- Frontend/src/utils/axios.jsx
+++ Frontend/src/utils/axios.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
+const DB_URL=import.meta.env.VITE_DB_URL;
 const instance=axios.create({
-    baseURL:"http://localhost:8000/api/v1",
+    baseURL:DB_URL || "http://localhost:8000/api/v1",
     withCredentials: true,
 })
 export default instance
